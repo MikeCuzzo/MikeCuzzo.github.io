1d53c669ff1dc4fc1e41e2acc6ddc17212e069c0
Mike Cuzzo <mcuzzo@smu.edu>
2022-02-08 10:33:29 -0600

    Resume toggle complete

--- my-site/src/components/bio.jsx
+++ my-site/src/components/bio.jsx
@@ -20,8 +20,19 @@ export const Bio = props => {
     return <div className="container">
         <h2 className="mb-5" id="tabHeader">About Me</h2>
         <div className="row" id="bioDiv">
-            <img src={account.avatar_url} alt="my-photo" className="col-lg-3 mb-4 mx-auto w-25" id="myPhoto"/>
-            <p className="col-12 col-lg-8 text-start" id="bio">{account.bio}</p>
+            <img src={account.avatar_url} alt="my-photo" className="col-lg-3 mb-4 w-25" id="myPhoto" />
+            <div id="bio-text" className="col">
+                <p className="col-12 col-lg-8 text-start">
+                    I am a 4th-year student studying Computer Science at Southern Methodist University.
+                    I am an aspiring front-end software developer.
+                </p>
+
+                <p className="col-12 col-lg-8 text-start">
+                    My passion for front-development stems from the joy of creating what the users will
+                    see in their software. I feel a connection with the end users, and I like to put myself
+                    in their shoes and pretend like I am them when I navigate my program.
+                </p>
+            </div>
         </div>
     </div>
 }
--- my-site/src/components/home.jsx
+++ my-site/src/components/home.jsx
@@ -1,20 +1,9 @@
-import React from "react";
-import OOResume from '../Resumes/OO-Resume.pdf'
-import WDResume from '../Resumes/WD-Resume.pdf'
+import React, { useEffect, useState } from "react";
+import OOResume from '../Resumes/OO-Resume.pdf';
+import WDResume from '../Resumes/WD-Resume.pdf';
 
 export const Home = props => {
-    var displayOO = false;
-    var displayWD = false;
-
-    function renderOO() {
-        displayOO = true;
-        displayWD = false;
-    };
-
-    function renderWD() {
-        displayOO = false;
-        displayWD = true;
-    };
+    const [toDisplay, setDisplay] = useState("");
 
     return (
         <div className="container">
@@ -25,13 +14,13 @@ export const Home = props => {
                 see what projects I have worked on,
                 and provide links to contact me.
             </h5>
-            <h3 className="mt-5 mb-3">Checkout My Resumes</h3>
+            <h3 className="mt-5">Checkout My Resumes</h3>
             <div id="resume-buttons" className="d-flex justify-content-around w-50 mx-auto">
-                <button className="badge btn-primary" onClick={renderOO}>Object Oriented</button>
-                <button className="badge btn-primary" onClick={renderWD}>Web Developement</button>
+                <button className="badge btn-primary" onClick={() => setDisplay("OO")}>Object Oriented</button>
+                <button className="badge btn-primary" onClick={() => setDisplay("WD")}>Web Developement</button>
             </div>
-            <iframe src={OOResume} className="w-100 bg-none"></iframe>
-            <iframe src={WDResume} className="w-100 bg-none"></iframe>
+            {toDisplay === "OO" && <iframe src={OOResume} className="w-100 bg-none" />}
+            {toDisplay === "WD" && <iframe src={WDResume} className="w-100 bg-none" />}
         </div>
     )
 }
