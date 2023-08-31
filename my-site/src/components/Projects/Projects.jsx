d9a0698be0471ee7dc96925b6074935a32bc1dc9
Mike Cuzzo <mcuzzo@smu.edu>
2023-08-31 14:46:34 -0500

    Added Aylo

--- my-site/src/components/Projects/Projects.jsx
+++ my-site/src/components/Projects/Projects.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import htvlogo from "./Assets/htvlogo.png";
 import elydzlogo from "./Assets/elydzlogo.png";
 import picklemalllogo from "./Assets/picklemalllogo.png";
+import aylologo from "./Assets/aylologo.png";
 import "./Projects.scss";
 
 export class Projects extends React.Component {
@@ -14,7 +15,7 @@ export class Projects extends React.Component {
       link: "https://www.headtovegas.com/",
     },
     {
-      name: "Elydz",
+      name: "Elydz Portfolio Website",
       description:
         "Eldyz, a rising music artist making waves in the industry. Immerse yourself in the unique style and artistry of this talented musician through their official website. Listen to their latest releases, and get to know the artist behind the music. Join the elydz community and stay up-to-date on all the latest news and releases from this exciting artist.",
       image: elydzlogo,
@@ -26,6 +27,13 @@ export class Projects extends React.Component {
         "The PickleMall website is designed to provide users with an effortless online experience, ensuring easy access to our cutting-edge indoor pickleball facilities. By allowing players to book courts, view schedules, and stay up-to-date on the latest events and promotions, our website is the go-to digital platform for pickleball enthusiasts. Connect with fellow players and secure your spot on the court today with the PickleMall website – your one-stop destination for all things pickleball.",
       image: picklemalllogo,
       link: "https://thepicklemall.com/",
+    },
+    {
+      name: "Ryan Dylan Selkirk Portfolio Website",
+      description:
+        "Ryan Dylan Selkirk, also known as Aylo—a visionary music video director who transforms auditory art into mesmerizing visual experiences. With a distinct knack for animation and an eye for aesthetic nuance, Aylo's work captures the essence of music in every frame. Explore the site to view a curated selection of projects, and journey through a landscape where sound meets vision.",
+      image: aylologo,
+      link: "https://www.ryandylanselkirk.com/",
     },
   ];
   render() {
